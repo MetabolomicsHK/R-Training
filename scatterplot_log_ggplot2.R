42b48c5b4684212ac069fb1e7f94e9047c08ef1f
wenbostar <wenbostar@gmail.com>
2014-05-14 15:17:11 +0800

    Update scatterplot_log_ggplot2.R

--- scatterplot_log_ggplot2.R
+++ scatterplot_log_ggplot2.R
@@ -2,7 +2,7 @@ a <- read.delim("../data/nature10098-s5.txt")
 daf <- data.frame(x=a$mRNA.copy.number.replicate..molecules.cell.,
                   y=a$Protein.copy.number.replicate..molecules.cell.)
 ggplot(data=daf,aes(x=x,y=y)) + 
-  geom_point(alpha=0.2) + 
+  geom_point(alpha=0.2) + ## geom_point(alpha=0.2,colour="blue")
   scale_y_log10() + 
   scale_x_log10() + 
   xlab("mRNA copies per cell, replicate") + 
