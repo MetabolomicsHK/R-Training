e935fd2220a2fb67f0f45363d111e166121b5427
wenbostar <wenbostar@gmail.com>
2014-05-14 15:42:21 +0800

    Update ErrorBarplot_ggplot2.R

--- ErrorBarplot_ggplot2.R
+++ ErrorBarplot_ggplot2.R
@@ -9,6 +9,6 @@ tg$Dose <- c(0.5,1,2)
 ggplot(tg, aes(x=factor(Dose), y=tg.m)) + 
     geom_bar(stat="identity", size=.2, width=0.3) +
     geom_errorbar(aes(ymin=tg.m-tg.sd, ymax=tg.m+tg.sd),
-                 	              size=0.3, width=.2)+
+        size=0.3, width=.2)+
     xlab("Dose") +
     ylab("Growth") 
