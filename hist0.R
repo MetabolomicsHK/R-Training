4214852ea92ccdc3c167dcc903e967d5573d4246
wenbostar <wenbostar@gmail.com>
2014-05-14 15:59:53 +0800

    Update hist0.R

--- hist0.R
+++ hist0.R
@@ -14,18 +14,18 @@ hist(x,
      font=2,cex.lab=1.2) #字体设置 
 #添加线条
 lines(density(x),
-      col="blue",lwd=2) 
+     col="blue",lwd=2) 
 lines(-30:0,
-      dnorm(-30:0,mean(x),sd(x)),
-      col="red",lwd=2) #同上
+     dnorm(-30:0,mean(x),sd(x)),
+     col="red",lwd=2) #同上
 #添加图例
 legend("topleft",
-       pch=c(15,-1,-1),
-       lty=c(-1,1,1),
-       col=c("gray","blue","red"),
-       legend=c("Histogram",
-                "Density line",
-                "Normal density line"))
+     pch=c(15,-1,-1),
+     lty=c(-1,1,1),
+     col=c("gray","blue","red"),
+     legend=c("Histogram",
+           "Density line",
+           "Normal density line"))
 
 
 
