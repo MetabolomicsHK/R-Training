476ec552fd7f6fecd0adebbc5afd6ae3bd5b036c
wenbostar <wenbostar@gmail.com>
2014-05-14 13:30:27 +0800

    Create dot_log_ggplot2.R

---
+++ dot_log_ggplot2.R
@@ -0,0 +1,8 @@
+
+a <- read.delim("../data/replicate_experiment_sRNA.txt")
+library("ggplot2")
+ggplot(data=a,aes(x=R1,y=RS)) + 
+  geom_point(alpha=0.2) + 
+  scale_y_log10() + 
+  scale_x_log10() +
+  ggtitle(label="R1 VS RS")
