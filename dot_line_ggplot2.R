0312d8a4b8c9de2e960ee6c18e4b837186cdefe7
wenbostar <wenbostar@gmail.com>
2016-07-19 00:12:05 +0800

    Update dot_line_ggplot2.R

--- dot_line_ggplot2.R
+++ dot_line_ggplot2.R
@@ -1,10 +1,8 @@
-
-
 a=read.table("../data/acgt.txt",head=T)
 library(ggplot2)
-library(reshape2)
+library(tidyr)
 a$Position <- 1:nrow(a)
-dat <- melt(a,value.name="Frequency",id.vars="Position",variable.name="Base")
+dat <- gather(a,"Base","Frequency",-Position)
 ggplot(data=dat,mapping=aes(x=Position,y=Frequency,shape=Base,colour=Base))+
   geom_line()+
   geom_point()+
